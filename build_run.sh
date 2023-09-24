528daea2f8b580343c439c55653c73552972811e
M-S-Dhanushkumar <dhanushkumar2811@gmail.com>
2023-09-24 21:01:14 +0530

    add build_run.sh

---
+++ build_run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Clone the GitHub repo
+git clone https://github.com/M-S-Dhanushkumar/my_web_app.git
+
+# Build the Docker image
+docker build -t dhanushkumar28/my_web_app:latest .
+
+# Authenticate to Docker Hub (you'll be prompted for your Docker Hub credentials)
+docker login
+
+# Push the Docker image to Docker Hub
+docker push dhanushkumar28/my_web_app:latest
+
+# Run the Docker container
+docker run -d -p 80:80 --name my_web_app_container dhanushkumar28/my_web_app:latest
