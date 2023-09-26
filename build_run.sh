444edb570624b8f717639b822e33edfe5b760aa7
M-S-Dhanushkumar <dhanushkumar2811@gmail.com>
2023-09-26 07:39:11 +0530

    modified .sh

--- build_run.sh
+++ build_run.sh
@@ -12,5 +12,9 @@ docker login
 # Push the Docker image to Docker Hub
 docker push dhanushkumar28/my_web_app:latest
 
+docker stop my_web_app_container 
+
+docker container rm my_web_app_container
+
 # Run the Docker container
 docker run -d -p 80:80 --name my_web_app_container dhanushkumar28/my_web_app:latest
