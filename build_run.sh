b3be31cb733f91cda6945e0df9dee5c0ed76645e
M-S-Dhanushkumar <dhanushkumar2811@gmail.com>
2023-09-27 08:22:54 +0530

    updated the shell and html

--- build_run.sh
+++ build_run.sh
@@ -3,33 +3,42 @@
 # Clone the GitHub repo
 #git clone https://github.com/M-S-Dhanushkumar/my_web_app.git
 
-cd my_web_app
+cd /home/dhanush/my_web_app/my_web_app
 
+VERSION=$(git log -1 --pretty=%h)
+REPO="dhanushkumar28/my_web_app:"
+TAG="$REPO$VERSION"
+LATEST="${REPO}latest"
 BUILD_TIMESTAMP=$( date '+%F_%H:%M:%S' )
 
 echo "Time: $BUILD_TIMESTAMP"
+pwd
 
 REMOTE=origin
 BRANCH=main
 git fetch
 if [[ "$(git rev-parse $BRANCH)" != "$(git rev-parse "$REMOTE/$BRANCH")" ]]; then
-  # Run your script
+echo "Found Changes"
+
+# Run your script
 git pull 
 # Build the Docker image
-docker build -t dhanushkumar28/my_web_app:latest .
+docker build -t "$TAG" -t "$LATEST" --build-arg VERSION="$VERSION" --build-arg BUILD_TIMESTAMP="$BUILD_TIMESTAMP" . 
 
 # Authenticate to Docker Hub (you'll be prompted for your Docker Hub credentials)
 docker login
 
 # Push the Docker image to Docker Hub
-docker push dhanushkumar28/my_web_app:latest
+#docker push dhanushkumar28/my_web_app:latest
+docker push "$TAG"
+docker push "$LATEST"
 
 docker stop my_web_app_container 
 
 docker container rm my_web_app_container
 
 # Run the Docker container
-docker run -d -p 80:80 --name my_web_app_container dhanushkumar28/my_web_app:latest
+docker run -d -p 80:80 --name my_web_app_container dhanushkumar28/my_web_app:"$VERSION"
 else
 	echo "No change on $REMOTE/$BRANCH"
 fi
